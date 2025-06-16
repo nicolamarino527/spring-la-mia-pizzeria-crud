b52cc6390babb0fdca45cf17506741aa2503f212
nicolamarino <nicolamarino527@gmail.com>
2025-06-16 20:41:25 +0200

    [add] post function

--- src/main/java/org/lesson/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lesson/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,10 +7,15 @@ import org.lesson.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -32,7 +37,7 @@ public class PizzaController {
     public String show(Model model, @PathVariable("id") Integer pizzaId) {
 
         Pizza pizza = repository.findById(pizzaId).get();
-        model.addAttribute("pizzas", pizza);
+        model.addAttribute("pizza", pizza);
         return "pizzas/show";
 
     }
@@ -42,5 +47,24 @@ public class PizzaController {
         List<Pizza> pizzas = repository.findByNameContaining(name);
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+
+        return "pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String post(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "pizzas/create";
+        }
+
+        repository.save(formPizza);
+
+        return "redirect:/pizzas";
     }
 }
